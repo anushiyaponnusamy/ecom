5205d41ced6db4e9c44169f4a59f5236de23d98f
anushiyaponnusamy <anushiyaponnusamy@gmail.com>
2023-12-13 13:08:18 +0530

    index changes

--- index.js
+++ index.js
@@ -16,18 +16,24 @@ dotenv.config({
 connectToMongoDb();
 const { PORT } = process.env;
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    preflightContinue: true, // Allow redirection in preflight requests
+  })
+);
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb' }));
+
 app.use('/auth', authRouter)
 app.use('/category', categoryRouter)
 app.use('/product', productRouter)
 app.use('/wishlist', wishlistRouter)
 app.use('/cart', cartRouter)
-app.get("/", (req, res) => {
-  res.send({
-    message: "root:ok",
-  });
-});
+
+app.get("/", (req, res, next) => res.status(200).json({ root: "ok" }));
 app.post('/uploadImage', uploadMiddleware, (req, res) => {
   const imageUrl = req.imageUrl;
   console.log(imageUrl)
